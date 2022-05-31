8714ea6a6fc07d0a5e7d9129f245aed5e0e4d77c
James Heathcote <james.h@biffbangpow.com>
2022-05-31 09:38:11 +0100

    BBP-14 Add page anchors to CTA extension

--- src/CallToActionExtension.php
+++ src/CallToActionExtension.php
@@ -15,26 +15,30 @@ use UncleCheese\DisplayLogic\Forms\Wrapper;
 class CallToActionExtension extends DataExtension
 {
     private static $db = [
-        'LinkText' => 'Varchar',
-        'CTAType' => 'Enum("None,Link,Download,External,Email", "None")',
-        'LinkData' => 'Varchar'
+        'LinkText'   => 'Varchar',
+        'CTAType'    => 'Enum("None,Link,Download,External,Email", "None")',
+        'LinkData'   => 'Varchar',
+        'LinkAnchor' => 'Varchar',
     ];
 
     private static $has_one = [
         'DownloadFile' => File::class,
-        'Action' => SiteTree::class
+        'Action'       => SiteTree::class,
     ];
     private static $owns = [
-        'DownloadFile'
+        'DownloadFile',
     ];
 
     public function updateCMSFields(FieldList $fields)
     {
-        $fields->removeByName(['CTAType', 'DownloadFile', 'LinkData', 'LinkText', 'ActionID']);
+        $fields->removeByName(['CTAType', 'DownloadFile', 'LinkData', 'LinkText', 'ActionID', 'LinkAnchor']);
         $fields->addFieldsToTab('Root.Main', [
             DropdownField::create('CTAType', 'Call to action type',
                 singleton($this->owner->ClassName)->dbObject('CTAType')->enumValues()),
             Wrapper::create(TreeDropdownField::create('ActionID', 'Link to page', SiteTree::class))
+                ->displayIf("CTAType")->isEqualTo("Link")->end(),
+            TextField::create('LinkAnchor')
+                ->setDescription('Anchor to append to the end of the link (do not need to include #)')
                 ->displayIf("CTAType")->isEqualTo("Link")->end(),
             UploadField::create('DownloadFile', 'Download File')->setFolderName('Downloads')
                 ->displayIf('CTAType')->isEqualTo("Download")->end(),
@@ -42,7 +46,7 @@ class CallToActionExtension extends DataExtension
                 ->setDescription('External link, email address, etc.')
                 ->displayIf("CTAType")->isEqualTo("External")
                 ->orIf("CTAType")->isEqualTo("Email")->end(),
-            TextField::create('LinkText')->displayUnless("CTAType")->isEqualTo("None")->end()
+            TextField::create('LinkText')->displayUnless("CTAType")->isEqualTo("None")->end(),
         ]);
         parent::updateCMSFields($fields);
     }
@@ -58,6 +62,9 @@ class CallToActionExtension extends DataExtension
                 break;
             case 'Link':
                 $link = $this->owner->Action()->Link();
+                if ($this->owner->LinkAnchor !== '' && $this->owner->LinkAnchor !== null) {
+                    $link .= '#' . $this->owner->LinkAnchor;
+                }
                 break;
             case 'Download':
                 $link = $this->owner->DownloadFile()->Link();
